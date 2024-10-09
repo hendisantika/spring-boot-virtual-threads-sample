4795d122bea50a81e9408fad0e73f3bb93155ac8
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 09:40:21 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/virtualthreads/SpringBootVirtualThreadsSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.virtualthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVirtualThreadsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVirtualThreadsSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/virtualthreads/SpringBootVirtualThreadsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.virtualthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVirtualThreadsSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
