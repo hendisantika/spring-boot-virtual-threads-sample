63eabd31079a8abae5b0fba2698890c7b37785a8
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 09:43:13 +0700

    Add RequestProcessingService 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/virtualthreads/SpringBootVirtualThreadsSampleApplication.java
+++ src/main/java/id/my/hendisantika/virtualthreads/SpringBootVirtualThreadsSampleApplication.java
@@ -2,7 +2,9 @@ package id.my.hendisantika.virtualthreads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class SpringBootVirtualThreadsSampleApplication {
 
---
+++ src/main/java/id/my/hendisantika/virtualthreads/service/RequestProcessingService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.virtualthreads.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-virtual-threads-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/10/24
+ * Time: 09.42
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service
+public class RequestProcessingService {
+
+    private final Logger log = LoggerFactory.getLogger(RequestProcessingService.class);
+
+}
