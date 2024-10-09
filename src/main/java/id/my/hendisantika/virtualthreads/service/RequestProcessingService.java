5a6a013622cdb560c26a3ed15c06ebb141948e70
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 09:43:32 +0700

    Add greetingsWithAsyncProcessing in RequestProcessingService 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/virtualthreads/service/RequestProcessingService.java
+++ src/main/java/id/my/hendisantika/virtualthreads/service/RequestProcessingService.java
@@ -2,7 +2,10 @@ package id.my.hendisantika.virtualthreads.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +23,9 @@ public class RequestProcessingService {
 
     private final Logger log = LoggerFactory.getLogger(RequestProcessingService.class);
 
+    @Async
+    public CompletableFuture<String> greetingsWithAsyncProcessing() {
+        log.info("Async request processing. current thread: {}", Thread.currentThread());
+        return CompletableFuture.completedFuture("Hello, World!");
+    }
 }
